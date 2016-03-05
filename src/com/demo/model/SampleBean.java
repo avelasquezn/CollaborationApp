5c50d67f8e4437741e730500a809423c54155e65
avelasquezn <avelasquezn@gmail.com>
2016-03-05 17:15:00 -0500

    Added name to SampleBean

--- src/com/demo/model/SampleBean.java
+++ src/com/demo/model/SampleBean.java
@@ -4,4 +4,13 @@ package com.demo.model;
  * Created by PT116-ORACLE on 05/03/2016.
  */
 public class SampleBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

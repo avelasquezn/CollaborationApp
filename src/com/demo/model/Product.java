8d350dc59cdc4696f71be3fe2ae381bcda809dbc
poozt <poolzeta@gmail.com>
2016-03-05 17:34:43 -0500

    added class product

---
+++ src/com/demo/model/Product.java
@@ -0,0 +1,25 @@
+package com.demo.model;
+
+/**
+ * Created by PT116-ORACLE on 05/03/2016.
+ */
+public class Product {
+    private String code;
+    private String producto;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+}
